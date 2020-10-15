8e505d758a18e91bce2a47ccb28eb3114e663ff2
Grinder21 <spartawinner280@mail.ru>
2020-10-15 15:39:35 +0300

    121211

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-
-	phpinfo();
-	exit();
 	ini_set('display_errors', '1');
 	ini_set('display_startup_errors', '1');
 	error_reporting(E_ALL);
 	include_once('mysql.php');
 	
 	$mysql = new MySQL('localhost', 'root', 'password', 'team');
+
+	print_r($mysql);
 	
 	// get all posts
+	/*
 	try{
 		$posts = $mysql->get('users');
 		print_r($posts);
@@ -17,4 +17,5 @@
 	}catch(Exception $e){
 		echo 'Caught exception: ', $e->getMessage();
 	}
+	*/
 ?>
