351af9012d7117a168045cf9d5019f4fafadafad
Grinder21 <spartawinner280@mail.ru>
2020-10-15 16:02:29 +0300

    121211

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 // подключаемся к серверу
-ini_set("log_errors", 1);
-ini_set("error_log", "/tmp/php-error.log");
-error_log( "Hello, errors!" );
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 $$servername = "localhost";
 $username = "root";
 $password = "password";
