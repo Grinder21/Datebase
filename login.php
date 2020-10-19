c9b671e44825b09fdaf75654b9964b34ad12a295
Grinder21 <spartawinner280@mail.ru>
2020-10-19 14:40:51 +0300

    12121

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ $result = mysqli_query("SELECT * FROM users WHERE email={$email}");
 if (isset($_POST['submit'])) {
 	if ($_POST['login'] != "" && $_POST['password'] != "") {
 		 $mysqli->query("INSERT INTO users (email, password) VALUES ({$email}', MD5({$password}));");
-		  if($data['password'] === md5(md5($_POST['password']))) {
+		  if($data['password'] == md5(md5($_POST['password']))) {
     			setcookie("id", $data['id'], time()+60*60*24*30, "/");
         		setcookie("hash", $hash, time()+60*60*24*30, "/", null, null, true);
         		echo 'Авторизация прошла успешна!';
